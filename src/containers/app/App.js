c64c81b5869cee2a605fd8407f62257365dcb633
Chaitanya Katakam <kchaitu3011@gmail.com>
2020-11-18 11:23:49 +0530

    Added rerouting to homepage

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import Layout from "../layout/Layout";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Detail from "../detail/Detail";
 import { Component } from "react";
 import Movies from "../movies/Movies";
@@ -19,8 +19,13 @@ class App extends Component {
             <Route path="/name/:name"  component={Movies} />
             <Route path="/movies" component={(props) => <Movies type="movie" titleName="Latest Movies" {...props} />} />
             <Route path="/shows" component={(props) => <Movies type="series" titleName="Latest TV Shows" {...props}/>} />
-            <Route path="/" exact component={MoviesAndShows} />
-            <Route path="/cmdb" exact component={MoviesAndShows} />
+            <Route path="/" exact component={MoviesAndShows} />    
+            
+            <Route path="/cmdb" exact render = {()=>{
+                return(
+                  <Redirect to="/" />
+                );
+              }} />
             
             
           </Switch>
